3eb8a9f6f31fc788271eb420cad0b2c306d50d70
Claire <claire.github-309c@sitedethib.com>
2026-09-22 19:21:59 +0200

    [Experimental] Add support for sign-up + OAuth flow

--- app/controllers/oauth/authorizations_controller.rb
+++ app/controllers/oauth/authorizations_controller.rb
@@ -25,6 +25,24 @@ class OAuth::AuthorizationsController < Doorkeeper::AuthorizationsController
     ActiveModel::Type::Boolean.new.cast(params[key])
   end
 
+  def authenticate_resource_owner!
+    if truthy_param?('signup') && !current_user
+      session[:sign_up_app_id] = params[:client_id]
+
+      return redirect_to(new_user_registration_path)
+    end
+
+    super
+  end
+
+  def after_successful_authorization(context)
+    current_resource_owner.update(created_by_application_id: session[:sign_up_app_id]) if session[:sign_up_app_id] == context.pre_auth.client.id
+  end
+
+  def require_functional!
+    super unless truthy_param?('signup')
+  end
+
   def mfa_setup_path
     super({ oauth: true })
   end
